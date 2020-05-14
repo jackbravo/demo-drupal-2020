a929470bc872f86f84e071f192bcfdb3727be376
Joaquin Bravo Contreras <jackbravo@gmail.com>
2020-05-14 00:25:18 -0500

    Update code to query for articles

--- web/modules/custom/demo_rest/src/Plugin/rest/resource/ArticlesRestResource.php
+++ web/modules/custom/demo_rest/src/Plugin/rest/resource/ArticlesRestResource.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\demo_rest\Plugin\rest\resource;
 
+use Drupal\Core\Database\Connection;
+use Drupal\Core\StreamWrapper\PublicStream;
 use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
@@ -29,12 +31,28 @@ class ArticlesRestResource extends ResourceBase {
   protected $currentUser;
 
   /**
+   * The database connection
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The file system stream wrapper
+   *
+   * @var string
+   */
+  protected $fileDir;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
     $instance->logger = $container->get('logger.factory')->get('demo_rest');
     $instance->currentUser = $container->get('current_user');
+    $instance->database = $container->get('database');
+    $instance->fileDir = $container->get('stream_wrapper.public')->getDirectoryPath();
     return $instance;
   }
 
@@ -57,7 +75,28 @@ class ArticlesRestResource extends ResourceBase {
             throw new AccessDeniedHttpException();
         }
 
-        return new ResourceResponse($payload, 200);
+        try {
+          $query = $this->database->select('node_field_data', 'nfd');
+          $query->condition('nfd.type', 'article');
+    
+          $query->join('node__field_image', 'n_fi', 'n_fi.entity_id = nfd.nid');
+          $query->join('file_managed', 'f', 'f.fid = n_fi.field_image_target_id');
+    
+          $query->addField('nfd', 'title');
+          $query->addExpression("REPLACE(f.uri, 'public:/', :base_path)", 'image', [':base_path' => $this->fileDir]);
+    
+          // $string = $query->execute()->getQueryString();
+    
+          $articles = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+    
+          $response = new ResourceResponse(['articles' => $articles]);
+          $response->setMaxAge(strtotime('1 day', 0));
+    
+          return $response;
+        }
+        catch (\Exception $e) {
+          throw new BadRequestHttpException($this->t('Could not find articles'), $e);
+        }
     }
 
 }
